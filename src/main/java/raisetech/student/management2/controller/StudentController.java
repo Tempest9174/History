ad2caae34650ea3a2a5c860be54386b69ee82106
masa <tailtikirinb1cs@gmail.com>
2024-12-15 17:21:13 +0900

    レッスン30POSTMAN

--- src/main/java/raisetech/student/management2/controller/StudentController.java
+++ src/main/java/raisetech/student/management2/controller/StudentController.java
@@ -2,7 +2,9 @@ package raisetech.student.management2.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,13 +12,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import raisetech.student.management2.controller.converter.StudentConverter;
 import raisetech.student.management2.data.StudentsCourses;
 import raisetech.student.management2.data.Student;
 import raisetech.student.management2.domain.StudentDetail;
 import raisetech.student.management2.service.StudentService;
 
-@Controller
+@RestController
 public class StudentController {
   private StudentService service;
   private StudentConverter converter;
@@ -30,14 +34,14 @@ public class StudentController {
 
 
   @GetMapping("/studentList")
-  public String  getStudentList(Model model) {
+  public List<StudentDetail>  getStudentList() {
     //StudentDetailにまとめるのが依然と異なる。
     List<Student> students = service.searchStudentList();
     //生徒リストを取得
     List<StudentsCourses> cours = service.searchCourseList();
-    model.addAttribute("studentList",converter.convertStudentDetails(students, cours));//コースリストを取得
+   // model.addAttribute("studentList",);//コースリストを取得
 
-    return "studentList";
+    return converter.convertStudentDetails(students, cours);
 
     //変数でなくStudent studentなのか？
     //表示
@@ -85,19 +89,20 @@ public class StudentController {
     }
 //難しい箇所👆AIツールの使い方
 
-//上のメソッド何してるか？
+//下、レッスン33
     @PostMapping("/updateStudent")
-    public String updateStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result) {
+    public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
 
-      if (result.hasErrors()) {
-        return "updateStudent";
-      }
+
+
    service.updateStudent(studentDetail);
    System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
-    return "redirect:/studentList";
+    return ResponseEntity.ok("更新処理が成功しました");
   }
-  @GetMapping("/courseList/{studentId}")
-  public List<StudentsCourses> getCourseList(@PathVariable Long studentId) {
+
+
+    @GetMapping("/courseList/{studentId}")
+    public List<StudentsCourses> getCourseList(@PathVariable Long studentId) {
     return service.searchCourseList();
     //引数消した
   }
